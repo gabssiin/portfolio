ac32fcf04eef373ac99cb0c49cfe0249cd4bab34
gabssiin <gabriel.willian.br@gmail.com>
2025-05-17 21:36:16 -0300

    Atualiza estilos e configurações do projeto para usar a fonte Lexend e ajusta cores no tema

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lexend } from '@next/font/google';
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const lexend = Lexend({
+  subsets: ['latin'], 
+  variable: '--font-lexend', 
 });
 
 export const metadata = {
@@ -19,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${lexend.className} antialiased`}> {}
         {children}
       </body>
     </html>
