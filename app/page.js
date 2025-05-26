605d1c72d0be217ef6d252a0ba0554dadd847da2
Gabriel Santos <163359098+gabssiin@users.noreply.github.com>
2025-05-26 23:49:00 +0000

    Refatora páginas de Contato, Home e Tecnologias com melhorias de layout e componentes

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="flex h-screen w-screen bg-gradient-to-r from-slate-950 to-slate-900">
       <SideMenu/>
-      <div className="relative rounded-xl overflow-hidden w-2/10 h-8/10 top-22 left-1 scale-90 transition-all duration-300 hover:scale-100">
+      <div className="relative rounded-xl overflow-hidden w-5/20 h-8/10 top-22 left-1 scale-90 transition-all duration-300 hover:scale-100">
         <Image
           src="/portfolio.jpg"
           alt="imagem da tela inicial"
@@ -23,23 +23,23 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col justify-center items-start w-1/2 text-white p-16">
-        <h1 className="text-6xl font-bold mb-10"> Gabriel Santos </h1>
-        <p className="text-lg mb-10">
+        <h1 className="text-8xl font-bold mb-10"> Gabriel Santos </h1>
+        <p className="text-2xl mb-10 ml-1">
           {" "}
           Olá, esse é o meu portfólio para os meus projetos.{" "}
         </p>
-        <div className="flex space-x-6">
+        <div className="flex space-x-6 ml-2">
           <a
             href="#"
             className="hover:opacity-100 bg-gradient-to-r from-cyan-500 to-cyan-400 scale-110 transition-all duration-300 hover:scale-130"
           >
-            <FaLinkedin size={36} color="202C39" />
+            <FaLinkedin size={50} color="00063D" />
           </a>
           <a
             href="#"
             className="hover:opacity-100 bg-gradient-to-r from-cyan-500 to-cyan-400 scale-110 transition-all duration-300 hover:scale-130"
           >
-            <FaSquareGithub size={36} color="202C39" />
+            <FaSquareGithub size={50} color="00063D" />
           </a>
         </div>
       </div>
--- app/tecnologias/page.js
+++ app/tecnologias/page.js
@@ -1,15 +1,16 @@
 // app/sobremim/page.js
 "use client";
 
-import React from 'react';
-
+import React from "react";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import SideMenu from "../components/SideMenu";
+import LoGo from "../components/LoGo";
 export default function tecnologias() {
   return (
-    <div>
-      <h1>tecnologias</h1>
-      {/* Adicione o conteúdo da sua página "Sobre mim" aqui */ 
-      <h1> Opa </h1>
-      }
+    <div className="min-h-screen bg-gradient-to-r from-slate-950 to-slate-900 flex flex-col items-center justify-center p-10 relative">
+      <MdOutlineWorkOutline size={30} color="white" className="mr-4" />
+      <h1 className="text-6xl mb-30">Tecnologias Usadas</h1>
+      {<h1 className="text-xl">React, TailwindCss, Next.js</h1>}
     </div>
   );
 }
